97797368dcd9c181b0b579b00137b213f36c6c3d
jamesoyanna <engrjayt200@gmail.com>
2023-05-11 16:06:03 +0100

    Updae stying

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState, useRef } from 'react';
-import { ContentState, convertToRaw, convertFromRaw  } from 'draft-js';
-import { EditorState} from 'draft-js';
+import { ContentState, convertToRaw, convertFromRaw } from 'draft-js';
+import { EditorState } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
-import  ImagePlugin from 'draft-js-image-plugin';
+import ImagePlugin from 'draft-js-image-plugin';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import './App.css';
+import Select from 'react-select';
 
 const imagePlugin = ImagePlugin();
 
@@ -13,9 +14,9 @@ function App() {
   const raw = convertToRaw(_contentState);
   const contentState = convertFromRaw(raw); // Convert raw JSON to ContentState
   const [editorState, setEditorState] = useState(EditorState.createWithContent(contentState));
-
   const [selectedImage, setSelectedImage] = useState(null);
   const fileInputRef = useRef(null);
+  const [showDropdown, setShowDropdown] = useState(false);
 
   const handleEditorChange = (editorState) => {
     setEditorState(editorState);
@@ -26,14 +27,14 @@ function App() {
       // Create a new FormData object
       const formData = new FormData();
       formData.append('file', file); // Append the selected file to the FormData
-  
+
       // Add your Cloudinary upload preset name
       const uploadPreset = 'wazobia';
       formData.append('upload_preset', uploadPreset); // Append the upload preset to the FormData
-  
+
       // Add your Cloudinary cloud name
       const cloudName = 'startupbuz';
-  
+
       // Make a POST request to the Cloudinary upload API
       const response = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
         method: 'POST',
@@ -42,11 +43,11 @@ function App() {
           'X-Requested-With': 'XMLHttpRequest',
         },
       });
-  
+
       if (response.ok) {
         const data = await response.json();
         const imageUrl = data.secure_url; // Get the secure URL of the uploaded image
-  
+
         // Set the selected image with the Cloudinary URL
         setSelectedImage(imageUrl);
       } else {
@@ -56,79 +57,96 @@ function App() {
       console.error('Image upload error:', error);
     }
   };
-  
 
   const handleButtonClick = () => {
-    fileInputRef.current.click();
+    setShowDropdown(!showDropdown);
   };
+  
+  
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     handleImageUpload(file);
   };
 
+
+  const handleOptionClick = (option) => {
+    console.log('Selected Option:', option);
+    setShowDropdown(false);
+  };
+  
+
   return (
     <div className="App">
       <div className="Editor-wrapper">
-      <header className="App-header">This is the title</header>
-      <div className="Editor-container">
-        <Editor
-          editorState={editorState}
-          onEditorStateChange={handleEditorChange}
-          wrapperClassName="wrapper-class"
-          editorClassName="editor-class"
-          toolbarClassName="toolbar-class"
-          toolbar={{
-            options: ['blockType', 'link', 'image', 'textAlign', 'inline', 'list'],
-            blockType: {
-              inDropdown: true,
-              options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'],
-            },
-            inline: {
-              options: ['bold', 'italic'],
-            },
-            list: {
-              options: ['ordered', 'unordered', 'indent'],
-            },
-            link: {
-              options: ['link'],
-            },
-            image: {
-              className: 'custom-option-class',
-              uploadEnabled: false, // Disable image upload via toolbar
-              alt: { present: true, mandatory: true },
-              previewImage: true,
-              alignmentEnabled: true,
-              defaultSize: {
-                width: '100%',
-                height: 'auto',
+        <header className="App-header">This is the title</header>
+        <div className="Editor-container">
+          <Editor
+            editorState={editorState}
+            onEditorStateChange={handleEditorChange}
+            wrapperClassName="wrapper-class"
+            editorClassName="editor-class"
+            toolbarClassName="toolbar-class"
+            toolbar={{
+              options: ['blockType', 'link', 'image', 'textAlign', 'inline', 'list'],
+              blockType: {
+                inDropdown: true,
+                options: ['Normal', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'],
               },
-            },
-            textAlign: {
-              options: ['left', 'center', 'right'],
-            },
-          }}
-          plugins={[imagePlugin]}
-        >
-          
-        </Editor>
-      
-        <input
-          type="file"
-          ref={fileInputRef}
-          style={{ display: 'none' }}
-          onChange={handleFileChange}
-        />
+              inline: {
+                options: ['bold', 'italic'],
+              },
+              list: {
+                options: ['ordered', 'unordered', 'indent'],
+              },
+              link: {
+                options: ['link'],
+              },
+              image: {
+                className: 'custom-option-class',
+                uploadEnabled: false, // Disable image upload via toolbar
+                alt: { present: true, mandatory: true },
+                previewImage: true,
+                alignmentEnabled: true,
+                defaultSize: {
+                  width: '100%',
+                  height: 'auto',
+                },
+              },
+              textAlign: {
+                options: ['left', 'center', 'right'],
+              },
+            }}
+            plugins={[imagePlugin]}
+          ></Editor>
+
+          <input
+            type="file"
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+            onChange={handleFileChange}
+          />
         </div>
         {selectedImage && (
-            <div>
-               <img src={selectedImage} alt="Selected" height="150px" width="150px" />
-            </div>
-          )}
+          <div>
+            <img src={selectedImage} alt="Selected" height="150px" width="150px" />
+          </div>
+        )}
       </div>
       <button onClick={handleButtonClick}>+</button>
+       {/* Dropdown */}
+       {showDropdown && (
+          <div className="dropdown-container">
+            <ul className="dropdown-options">
+              <li onClick={() => handleOptionClick('image')}>Upload Image</li>
+              <li onClick={() => handleOptionClick('video')}>Upload Video</li>
+              <li onClick={() => handleOptionClick('social-media')}>Upload Social Media Link</li>
+            </ul>
+          </div>
+        )}
     </div>
   );
+
 }
 
 export default App;
