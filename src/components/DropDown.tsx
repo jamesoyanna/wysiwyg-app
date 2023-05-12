df7f95ffbc8be9344180a7bd8953f6b022489105
jamesoyanna <engrjayt200@gmail.com>
2023-05-12 12:43:25 +0100

    Refactor Dropdown component

--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,38 +1,47 @@
 import React from 'react';
-import PictureIcon from '../icons/picture-icon.png';
-import VideoIcon from '../icons/video-icon.png';
-import SocialIcon from '../icons/facebook-con.svg';
+import DropdownOptions from './DropdownOptions';
+
 
 interface DropdownProps {
   handleOptionClick: (option: string) => void;
 }
 
 const Dropdown: React.FC<DropdownProps> = ({ handleOptionClick }) => {
+  const options = [
+    {
+      option: 'image',
+      icon: require('../icons/picture-icon.png'),
+      title: 'Picture',
+      description: 'JPEG, PNG',
+    },
+    {
+      option: 'video',
+      icon: require('../icons/video-icon.png'),
+      title: 'Video',
+      description: 'Embed a YouTube video',
+    },
+    {
+      option: 'social-media',
+      icon: require('../icons/facebook-circle.png'),
+      title: 'Social',
+      description: 'Embed a Facebook link',
+    },
+  ];
+
   return (
     <div className="dropdown-container">
       <h2 className="dropdown-title">EMBEDS</h2>
       <ul className="dropdown-options">
-        <li onClick={() => handleOptionClick('image')}>
-          <img src={PictureIcon} alt="icon" className="option-icon" />
-          <div>
-            <span className="option-title">Picture</span>
-            <p className="option-description">JPEG, PNG</p>
-          </div>
-        </li>
-        <li onClick={() => handleOptionClick('video')}>
-          <img src={VideoIcon} alt="Video" className="option-icon" />
-          <div>
-            <span className="option-title">Video</span>
-            <p className="option-description">Embed a YouTube video</p>
-          </div>
-        </li>
-        <li onClick={() => handleOptionClick('social-media')}>
-          <img src={SocialIcon} alt="Social Media" className="option-icon" />
-          <div>
-            <span className="option-title">Social</span>
-            <p className="option-description">Embed a Facebook link</p>
-          </div>
-        </li>
+        {options.map((option) => (
+          <DropdownOptions
+            key={option.option}
+            option={option.option}
+            icon={option.icon}
+            title={option.title}
+            description={option.description}
+            handleOptionClick={handleOptionClick}
+          />
+        ))}
       </ul>
     </div>
   );
