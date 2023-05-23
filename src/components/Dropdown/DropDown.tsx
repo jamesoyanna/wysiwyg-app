83bc7f079e9d919339a6b2849bf4227401bf1cc6
jamesoyanna <engrjayt200@gmail.com>
2023-05-23 01:55:03 +0100

    Refactor imageupload component

--- src/components/Dropdown/DropDown.tsx
+++ src/components/Dropdown/DropDown.tsx
@@ -2,32 +2,39 @@ import React from 'react';
 import DropdownOptions from './DropdownOptions';
 import "./dropdown.css";
 
+interface Option {
+  option: string;
+  icon: string;
+  title: string;
+  description: string;
+}
+
 interface DropdownProps {
   handleOptionClick: (option: string) => void;
 }
 
-const Dropdown: React.FC<DropdownProps> = ({ handleOptionClick }) => {
-  const options = [
-    {
-      option: 'image',
-      icon: require('../../icons/picture-icon.png'),
-      title: 'Picture',
-      description: 'JPEG, PNG',
-    },
-    {
-      option: 'video',
-      icon: require('../../icons/video-icon.png'),
-      title: 'Video',
-      description: 'Embed a YouTube video',
-    },
-    {
-      option: 'social-media',
-      icon: require('../../icons/facebook-circle.png'),
-      title: 'Social',
-      description: 'Embed a Facebook link',
-    },
-  ];
+const options: Option[] = [
+  {
+    option: 'image',
+    icon: require('../../icons/picture-icon.png'),
+    title: 'Picture',
+    description: 'JPEG, PNG',
+  },
+  {
+    option: 'video',
+    icon: require('../../icons/video-icon.png'),
+    title: 'Video',
+    description: 'Embed a YouTube video',
+  },
+  {
+    option: 'social-media',
+    icon: require('../../icons/facebook-circle.png'),
+    title: 'Social',
+    description: 'Embed a Facebook link',
+  },
+];
 
+const Dropdown: React.FC<DropdownProps> = ({ handleOptionClick }) => {
   return (
     <div className="dropdown-container">
       <h2 className="dropdown-title">EMBEDS</h2>
@@ -35,11 +42,8 @@ const Dropdown: React.FC<DropdownProps> = ({ handleOptionClick }) => {
         {options.map((option) => (
           <DropdownOptions
             key={option.option}
-            option={option.option}
-            icon={option.icon}
-            title={option.title}
-            description={option.description}
             handleOptionClick={handleOptionClick}
+            {...option}
           />
         ))}
       </ul>
--- src/components/imageupload/ImageUploadModal.tsx
+++ src/components/imageupload/ImageUploadModal.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import './imageupload.css';
-import UploadIcon from '../../icons/upload.png'
-import CloseIcon from '../../icons/x.png'
+import UploadIcon from '../../icons/upload.png';
+import CloseIcon from '../../icons/x.png';
 
 const ImageUploadModal = ({ closeModal, isUploading, handleFileChange }) => {
-
   return (
     <div className="modal-overlay">
       <div className="modal-container">
         <div className="upload-area">
           <label htmlFor="file-upload" className="upload-label">
-            <img src={UploadIcon}  alt='Upload Icon' className="upload-icon"/>
+            <img src={UploadIcon} alt="Upload Icon" className="upload-icon" />
             {isUploading ? 'Uploading...' : ''}
           </label>
           <input type="file" id="file-upload" className="file-upload" onChange={handleFileChange} />
-        <p className="modal-title">Upload Image</p>
-        <button className="modal-close" onClick={closeModal}>
-          <img src={CloseIcon} alt='Close Icon'/>
-        </button>
+          <p className="modal-title">Upload Image</p>
+          <button className="modal-close" onClick={closeModal}>
+            <img src={CloseIcon} alt="Close Icon" />
+          </button>
         </div>
       </div>
     </div>
